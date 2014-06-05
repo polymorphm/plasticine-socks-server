4718695a688e35d408975e712c989ed4894aa684
Andrej Antonov <polymorphm@gmail.com>
2014-06-05 22:54:39 +0400

    'multi_channel_perm' was added to 'builtin_features'

--- lib_plasticine_socks_server_2014_02_08/main.py
+++ lib_plasticine_socks_server_2014_02_08/main.py
@@ -37,6 +37,7 @@ BUILTIN_FEATURE_NAMESPACE = '{}.builtin_features'.format(
 BUILTIN_FEATURE_NAME_LIST = (
         'addrinfo_appspot',
         'http_distortion',
+        'multi_channel_perm',
         )
 
 def get_feature_by_shortcut(feature_name):
