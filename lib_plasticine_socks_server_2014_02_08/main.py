10c98b49f59aa8596dff00e0fcf4b6487f566594
Andrej Antonov <polymorphm@gmail.com>
2016-03-08 15:20:02 +0300

    fixed header in 'main.py'

--- lib_plasticine_socks_server_2014_02_08/main.py
+++ lib_plasticine_socks_server_2014_02_08/main.py
@@ -1,6 +1,6 @@
 # -*- mode: python; coding: utf-8 -*-
 #
-# Copyright (c) 2014 Andrej Antonov <polymorphm@gmail.com>
+# Copyright (c) 2014, 2015, 2016 Andrej Antonov <polymorphm@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
